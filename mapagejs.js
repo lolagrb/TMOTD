41206635f46ebb006cc98779453c43bf193f175d
lolagrb <lola.grumbach@gmail.com>
2022-04-27 17:17:48 -0400

    avancement 2

--- mapagejs.js
+++ mapagejs.js
@@ -1,23 +1,37 @@
-<script>
-    fetch ("https://api.giphy.com/v1/gifs/random?api_key=QIrDmmXVWDKptBxoJZRjwODaycU7zvq9&tag=&rating=g")
-    .then((res) => res.json())
-    .then((obj)=>
-        const URL_gif = listObj.obj.data.images.original.mp4;
-        document.getElementById(gif).innerHTML= <video autoplay width="250">
+fetch ("https://api.giphy.com/v1/gifs/random?api_key=QIrDmmXVWDKptBxoJZRjwODaycU7zvq9&tag=&rating=g")
+.then((res) => {return res.json();})
+.then((obj)=> {
+    const URL_gif = obj.data.images.original.mp4;
+    document.getElementById("gif")
+    .innerHTML= `<video autoplay width="250">
+    <source
+        id="my-video-url"
+        src = ${URL_gif}
+        type="video/mp4"
+    />
+
+    Sorry, your browser doesn't support embedded videos.
+    </video>
+      `;
+
+  })
+      
+
+/* 
+ fetch ("https://api.giphy.com/v1/gifs/trending?api_key=QIrDmmXVWDKptBxoJZRjwODaycU7zvq9&limit=25&rating=g")
+    .then((res) => {return res.json();})
+    .then((obj)=> {
+        const URL_gif = obj.data.images.original.mp4;
+        document.getElementById("gif")
+        .innerHTML= `<video autoplay width="250">
         <source
             id="my-video-url"
             src = ${URL_gif}
             type="video/mp4"
         />
 
-
         Sorry, your browser doesn't support embedded videos.
         </video>
+          `;
 
-
-
-        )
-
-</script>
-
-
+      }) */
