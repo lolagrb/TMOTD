a71c8858ab6c1fdc08c33bc7b80f00bf3530bc7a
alexis anzieu <alexis.anzieu@ssense.com>
2022-06-01 18:54:41 -0400

    push id check

--- mapagejs.js
+++ mapagejs.js
@@ -1,9 +1,23 @@
-fetch ("https://api.giphy.com/v1/gifs/random?api_key=QIrDmmXVWDKptBxoJZRjwODaycU7zvq9&tag=&rating=g")
-.then((res) => {return res.json();})
-.then((obj)=> {
-    const URL_gif = obj.data.images.original.mp4;
-    document.getElementById("gif")
-    .innerHTML= `<video autoplay width="250">
+function randomizer(min, max) {
+    return Math.floor(Math.random() * max) + min
+}
+const LIMIT = 15;
+
+
+fetch(`https://api.giphy.com/v1/gifs/trending?api_key=QIrDmmXVWDKptBxoJZRjwODaycU7zvq9&rating=pg&limit=${LIMIT}`)
+    .then((res) => { return res.json(); })
+    .then((obj) => {
+        let gifChoiceNumber = randomizer(0, LIMIT);
+        const currentURL = window.location.href;
+        if (!currentURL.includes('gif')) {
+            window.location.href = `${window.location.href}?gif=${gifChoiceNumber}`
+        } else {
+            gifChoiceNumber = currentURL.split('=')[1];
+        }
+        console.log(obj);
+        const URL_gif = obj.data[gifChoiceNumber].images.original.mp4;
+        document.getElementById("gif")
+            .innerHTML = `<video autoplay loop width="250">
     <source
         id="my-video-url"
         src = ${URL_gif}
@@ -14,24 +28,4 @@ fetch ("https://api.giphy.com/v1/gifs/random?api_key=QIrDmmXVWDKptBxoJZRjwODaycU
     </video>
       `;
 
-  })
-      
-
-/* 
- fetch ("https://api.giphy.com/v1/gifs/trending?api_key=QIrDmmXVWDKptBxoJZRjwODaycU7zvq9&limit=25&rating=g")
-    .then((res) => {return res.json();})
-    .then((obj)=> {
-        const URL_gif = obj.data.images.original.mp4;
-        document.getElementById("gif")
-        .innerHTML= `<video autoplay width="250">
-        <source
-            id="my-video-url"
-            src = ${URL_gif}
-            type="video/mp4"
-        />
-
-        Sorry, your browser doesn't support embedded videos.
-        </video>
-          `;
-
-      }) */
+    })
