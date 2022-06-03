6dea60028eacd4fb1daf64a0989df7cb974fd7d8
lolagrb <lola.grumbach@gmail.com>
2022-06-03 15:17:30 -0400

    gif number replace by gif id

--- mapagejs.js
+++ mapagejs.js
@@ -7,12 +7,13 @@ const LIMIT = 15;
 fetch(`https://api.giphy.com/v1/gifs/trending?api_key=QIrDmmXVWDKptBxoJZRjwODaycU7zvq9&rating=pg&limit=${LIMIT}`)
     .then((res) => { return res.json(); })
     .then((obj) => {
-        let gifChoiceNumber = randomizer(0, LIMIT);
-        const currentURL = window.location.href;
-        if (!currentURL.includes('gif')) {
-            window.location.href = `${window.location.href}?gif=${gifChoiceNumber}`
+        let gifChoiceNumber = randomizer(0, LIMIT); /* pourquoi on utilise let ? */
+        let gifID = obj.data[gifChoiceNumber].id /*On vient récupérer et isoler dans une variable l'ID du gif */
+        const currentURL = window.location.href; /* On vient récupérer l'URL tel qu'affiché sur mon navigateur */
+        if (!currentURL.includes('gif')) {   
+            window.location.href = `${window.location.href}?gif=${gifID}`  /* Si l'URL ne contient PAS le mot gif alors on vient lui ajouter le parametre en indiquant la valeur qu'il doit prendre, ici l'ID*/
         } else {
-            gifChoiceNumber = currentURL.split('=')[1];
+            gifID = currentURL.split('=')[1];  /* Si l'URL contient le mot gif, alors on indique quel parametre il doit prendre */
         }
         console.log(obj);
         const URL_gif = obj.data[gifChoiceNumber].images.original.mp4;
